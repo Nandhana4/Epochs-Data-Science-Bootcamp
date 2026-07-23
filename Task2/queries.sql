85c6f9bbf763353441a439f73b669c4984f7b0ab
Nandhana S <nandhanasathyan267@gmail.com>
2026-07-23 20:02:43 +0530

    Add files via upload

---
+++ Task2/queries.sql
@@ -0,0 +1,15 @@
+
+-- Query 1: Top 10 Selling Products
+<PASTE QUERY 1 HERE>
+
+-- Query 2: Top 10 Customers by Revenue
+<PASTE QUERY 2 HERE>
+
+-- Query 3: Monthly Sales Trends
+<PASTE QUERY 3 HERE>
+
+-- Query 4: Best Performing Product Categories
+<PASTE QUERY 4 HERE>
+
+-- Query 5: Customer Purchase Frequency
+<PASTE QUERY 5 HERE>
